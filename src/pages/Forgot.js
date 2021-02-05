8732769b2408f709bb107d6d4a0c2a00df68aeec
RoMzz37 <78389631+RoMzz37@users.noreply.github.com>
2021-02-04 21:15:01 -0500

    Reset Password Page and Validation

--- src/pages/Forgot.js
+++ src/pages/Forgot.js
@@ -1,8 +1,38 @@
 import React from "react";
 import {IonContent, IonItem, IonPage, IonLabel, IonInput, IonRow, IonCol, IonButton} from "@ionic/react";
 import NavHeader from "../components/Header/NavHeader";
+import {toast} from "../utils/toast";
+import useFormValidation from "../hooks/useFormValidation";
+import validatePasswordReset from "../components/Auth/validatePasswordReset";
+import firebase from "../firebase";
 
-const Forgot = () => {
+const INITIAL_STATE = {
+    email: "",
+  };
+  
+const Forgot = (props) => {
+    const {
+        handleSubmit,
+        handleChange,
+        values,
+        isSubmitting
+      } = useFormValidation(INITIAL_STATE, validatePasswordReset, handleResetPassword)
+      const [busy, setBusy] = React.useState(false);
+
+      async function handleResetPassword(){
+        setBusy(true);
+        const {email} = values;
+        try {
+          await firebase.resetPassword(email);
+          toast("Check your email to reset your password");
+          props.history.push("/login");
+        } catch (err){
+          console.error("Password Reset Error", err);
+          toast(err.message);
+        }
+        setBusy(false);
+      }
+      
     return (
         <IonPage>
             <NavHeader title="Forgot"></NavHeader>
@@ -10,12 +40,12 @@ const Forgot = () => {
 
                 <IonItem lines="full">
                     <IonLabel position="floating">Email</IonLabel>
-                    <IonInput name="email" type="text" required></IonInput>
+                    <IonInput name="email" type="text" value={values.email} onIonChange={handleChange} required></IonInput>
                 </IonItem>
 
                 <IonRow>
                     <IonCol>
-                        <IonButton type="submit" color="primary" expand="block">
+                        <IonButton type="submit" color="primary" expand="block" onClick={handleSubmit} disabled={isSubmitting}>
                             Get Reset Link
                         </IonButton>
                     </IonCol>

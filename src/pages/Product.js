4d443ff653265a85ddacfe758c84852836b7e369
RoMzz37 <78389631+RoMzz37@users.noreply.github.com>
2021-02-13 00:55:59 -0500

    Product Comments

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,21 +1,30 @@
 import React from "react";
-import { IonPage, IonContent, IonGrid, IonRow, IonCol, IonButton } from "@ionic/react";
+import {
+  IonPage,
+  IonContent,
+  IonButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/react";
 import { closeCircleOutline } from "ionicons/icons";
-import productService from "../services/product";
 
 import firebase from "../firebase";
+import productService from "../services/product";
 import { Plugins } from "@capacitor/core";
 import UserContext from "../contexts/UserContext";
 import NavHeader from "../components/Header/NavHeader";
 import ProductItem from "../components/Product/ProductItem";
 import ProductPhotos from "../components/Product/ProductPhotos";
-
+import ProductComment from "../components/Product/ProductComment";
+import CommentModal from "../components/Product/CommentModal";
 
 const { Browser } = Plugins;
 
 const Product = (props) => {
   const { user } = React.useContext(UserContext);
   const [product, setProduct] = React.useState(null);
+  const [showModal, setShowModal] = React.useState(false);
   const productId = props.match.params.productId;
   const productRef = firebase.db.collection("products").doc(productId);
 
@@ -30,15 +39,51 @@ const Product = (props) => {
     });
   }
 
+  function handleOpenModal() {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      setShowModal(true);
+    }
+  }
+
+  function handleCloseModal() {
+    setShowModal(false);
+  }
+
+  function handleAddComment(commentText) {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      productRef.get().then((doc) => {
+        if (doc.exists) {
+          const previousComments = doc.data().comments;
+          const newComment = {
+            postedBy: { id: user.uid, name: user.displayName },
+            created: Date.now(),
+            text: commentText,
+          };
+          const updatedComments = [...previousComments, newComment];
+          productRef.update({ comments: updatedComments });
+          setProduct((prevState) => ({
+            ...prevState,
+            comments: updatedComments,
+          }));
+        }
+      });
+      setShowModal(false);
+    }
+  }
+
   function handleAddVote() {
-      if(!user) {
-          props.history.push("/login");
-      } else {
-          productService
-          .addUpvote(user, productId)
-          .then((newProduct) => setProduct(newProduct))
-          .catch(() => props.history.push("/login"))
-      }
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      productService
+        .addUpvote(user, productId)
+        .then((newProduct) => setProduct(newProduct))
+        .catch(() => props.history.push("/login"));
+    }
   }
 
   function handleDeleteProduct() {
@@ -72,6 +117,12 @@ const Product = (props) => {
         action={handleDeleteProduct}
       />
       <IonContent>
+        <CommentModal
+          isOpen={showModal}
+          title="New Comment"
+          sendAction={handleAddComment}
+          closeAction={handleCloseModal}
+        />
         {product && (
           <>
             <IonGrid>
@@ -80,11 +131,22 @@ const Product = (props) => {
                   <ProductItem product={product} browser={openBrowser} />
                   <ProductPhotos photos={product.photos} />
                   <IonButton onClick={() => handleAddVote()} size="small">
-                      Upvote
+                    Upvote
+                  </IonButton>
+                  <IonButton onClick={() => handleOpenModal()} size="small">
+                    Comment
                   </IonButton>
                 </IonCol>
               </IonRow>
             </IonGrid>
+            {product.comments.map((comment, index) => (
+              <ProductComment
+                key={index}
+                comment={comment}
+                product={product}
+                setProduct={setProduct}
+              />
+            ))}
           </>
         )}
       </IonContent>
